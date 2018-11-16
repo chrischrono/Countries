ecd8086d98cdb906345dc467ecf9571415624668
Christian Huang <christian@christians-imac.local>
2018-11-17 05:02:34 +0700

    Design CountriesViewController on StoryBoard

--- Countries/View/CountriesViewController.swift
+++ Countries/View/CountriesViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class CountriesViewController: UIViewController {
-
+    @IBOutlet private weak var countriesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+}
+
+extension CountriesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
---
+++ Countries/View/ViewCell/CountryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CountryCell.swift
+//  Countries
+//
+//  Created by Christian  Huang on 17/11/18.
+//  Copyright © 2018 Christian Huang. All rights reserved.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+    
+    @IBOutlet private weak var flagImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var regionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
